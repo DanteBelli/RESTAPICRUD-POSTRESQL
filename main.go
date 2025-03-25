a563243a94b468dc9d4d6843bc8eb06605060bf2
Bell <dante.be@hotmail.com>
2025-03-25 12:21:49 -0300

    Se agreegan los archivo struct de user y task , se agrega el db connection y modifica el main para ejecutar este mismo

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/Dante-Belli/RESTAPICRUD-POSTRESQL/db"
 	"github.com/Dante-Belli/RESTAPICRUD-POSTRESQL/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.DBConnection()
+
 	router := mux.NewRouter()
+
 	router.HandleFunc("/", routes.Hola)
+
 	http.ListenAndServe(":3000", router)
 }
---
+++ routes/users.routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/Dante-Belli/RESTAPICRUD-POSTRESQL/models"
+)
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	var users []models.User
+
+}
