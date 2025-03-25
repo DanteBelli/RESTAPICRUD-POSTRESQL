1b8e1a9a6567212121f30831caa97e02eba25491
Bell <dante.be@hotmail.com>
2025-03-25 13:00:48 -0300

    Se agrega los migrates de la db en el main

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 
 	"github.com/Dante-Belli/RESTAPICRUD-POSTRESQL/db"
+	"github.com/Dante-Belli/RESTAPICRUD-POSTRESQL/models"
 	"github.com/Dante-Belli/RESTAPICRUD-POSTRESQL/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+
+	db.DB.AutoMigrate(models.User{})
 
 	router := mux.NewRouter()
 
